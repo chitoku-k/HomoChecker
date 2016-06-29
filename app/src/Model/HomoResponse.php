16266c8e4972e5d1c3deeda39fbb4ff708d3b8ad
Chitoku <odango@chitoku.jp>
2016-06-30 00:30:31 +0900

    Add fields: display_url with punycode converted, and secure

--- app/src/Model/HomoResponse.php
+++ app/src/Model/HomoResponse.php
@@ -1,14 +1,30 @@
 <?php
 namespace HomoChecker\Model;
 
+use TrueBV\Punycode;
+
 class HomoResponse {
     public function __construct(Homo $homo, Icon $icon, string $status, float $duration) {
         $this->homo = [
             'screen_name' => $homo->screen_name,
             'url'         => $homo->url,
+            'display_url' => $this->createDisplayURL($homo->url),
+            'secure'      => $this->isSecure($homo->url),
             'icon'        => $icon->url,
         ];
         $this->status = $status;
         $this->duration = round($duration, 2);
+    }
+
+    protected function createDisplayURL(string $url): string {
+        if (!preg_match('/(https?:\/\/)(.*)(:\d+)?(\/.*)?/', $url, $matches)) {
+            return '';
+        }
+        list(, $protocol, $domain, $port, $path) = $matches;
+        return (new Punycode)->decode($domain) . $path;
+    }
+
+    protected function isSecure(string $url): bool {
+        return strpos($url, 'https://') === 0;
     }
 }
