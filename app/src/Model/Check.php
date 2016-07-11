16ab289e563ed2cb968dd640a5eb173326df99fa
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-07-11 17:08:50 +0900

    Use CURLINFO_TOTAL_TIME instead of microtime()

--- app/src/Model/Check.php
+++ app/src/Model/Check.php
@@ -40,36 +40,35 @@ class Check
         return $ch;
     }
 
-    protected function timer(): float
-    {
-        return microtime(true) - $this->time;
-    }
-
     protected function validate(Homo $homo): \Generator
     {
         list($header_validator) = $this->validators;
+
         yield $ch = $this->initialize($homo->url, false);
+        $time = curl_getinfo($ch, CURLINFO_TOTAL_TIME);
+
         if (($status = $header_validator($ch, ''))) {
-            return [$status, $this->timer()];
+            return [$status, $time];
         }
 
         $body = yield $ch = $this->initialize($homo->url, true);
+        $time = curl_getinfo($ch, CURLINFO_TOTAL_TIME);
+
         if ($body instanceof CURLException || !curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
-            return ['ERROR', $this->timer()];
+            return ['ERROR', $time];
         }
 
         foreach ($this->validators as $validator) {
             if (($status = $validator($ch, $body))) {
-                return [$status, $this->timer()];
+                return [$status, $time];
             }
         }
 
-        return ['WRONG', $this->timer()];
+        return ['WRONG', $time];
     }
 
     public function execute(string $screen_name = null, callable $callback = null): array
     {
-        $this->time = microtime(true);
         $homos = isset($screen_name) ? Homo::getByScreenName($screen_name) : Homo::getAll();
 
         return Co::wait(array_map(function ($homo) use ($callback): \Generator {
