0e57d281678dce288a71648954783c54567c646f
Chitoku <odango@chitoku.jp>
2016-07-11 12:47:15 +0900

    Update Homo

--- app/src/Model/Homo.php
+++ app/src/Model/Homo.php
@@ -7,62 +7,42 @@ class Homo
     public $url;
 
     private static $sites = [
-        'java_shit' => [
-            'https://homo.chitoku.jp',
-            'http://xn--ydko.chitoku.jp',
+        'azyobuzin' => [
+            'http://homo.azyobuzi.net',
+            'http://xn--ydko.azyobuzi.net',
         ],
-        // 'tuyapin' => 'http://homo.tuyapin.net',
+        'CHIKEN_MAN_' => 'http://homo.xn--w8jwb2eudb.com',
+        'G2U' => 'http://homo.mohyo.net',
+        'Hexium310' => [
+            'https://hexium310.github.io/homo',
+            'http://homo.hexium.xyz',
+        ],
         'hnle0' => [
             'https://homo.hinaloe.net',
             'http://homo.hnle.tk',
             'http://xn--ydko.hinaloe.net',
             'http://xn--ydko.hnle.tk',
         ],
-        'azyobuzin' => [
-            'http://homo.azyobuzi.net',
-            'http://xn--ydko.azyobuzi.net',
+        'homomaid' => 'http://homo.homomaid.com',
+        'java_shit' => [
+            'https://homo.chitoku.jp',
+            'http://xn--ydko.chitoku.jp',
         ],
-        'u3g3' => [
-            'https://homo.gomasy.jp',
-            // 'https://homo.goma.ga',
-        ],
-        'G2U' => 'http://homo.mohyo.net',
-        'Hexium310' => [
-            'https://hexium310.github.io/homo',
-            'http://homo.hexium.xyz',
-        ],
-        // 'DYGV' => [
-        //    'http://homo.dygv.org',
-        //    'http://xn--ydko.dygv.org',
-        //    'http://xn--79jo.dygv.org',
-        // ],
-        'shibafu528' => 'http://homo.shibafu528.info',
         'kb10uy' => [
             'http://homo.kb10uy.org',
             'http://xn--ydko.kb10uy.org',
         ],
-        'myskng' => 'http://homo.kazukioishi.net',
-        'tk1024_bot' => 'http://homo.tk1024.net',
-        // 'nkpoid' => 'http://homo.nkpoid.pw',
-        'Petitsurume' => 'https://petitsurume.github.io/homo/',
-        // 'sudosan' => [
-        //    'https://homo.sudosan.net',
-        //    'https://homo.114514.jp',
-        //    'http://homo.syaro.me',
-        //    'http://homo.megune.com',
-        //    'http://homo.chino.pw',
-        //    'http://homo.chiya.pw',
-        //    'http://homo.kokoa.pw',
-        //    'http://homo.rize.pw',
-        // ],
-        '491MHz' => 'http://homo.491mhz.pw',
-        'printf_moriken' => 'http://moriken.kimamass.com/homo',
-        'homomaid' => 'http://homo.homomaid.com',
         'LaLN_' => [
             'https://homo.lunasys.tk',
             'https://homo.synchthia.net',
         ],
-        // 'junT58' => 'http://homo.junt58.tk',
+        'myskng' => 'http://homo.kazukioishi.net',
+        'pakutoma' => 'http://homo.pakutoma.pw',
+        'Petitsurume' => 'https://petitsurume.github.io/homo/',
+        'printf_moriken' => 'http://moriken.kimamass.com/homo',
+        'shibafu528' => 'http://homo.shibafu528.info',
+        'tk1024_bot' => 'http://homo.tk1024.net',
+        'u3g3' => 'https://homo.gomasy.jp',
     ];
 
     public static function getAll(): array
