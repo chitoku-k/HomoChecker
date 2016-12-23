aea65fbfa0a11b3c90922d83c1d400033a78c742
Chitoku <odango@chitoku.jp>
2016-12-23 20:28:15 +0900

    Create sample configuration instead

--- app/src/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-const DB_HOST = '<your database host>';
-const DB_NAME = '<your database name>';
-const DB_USER = '<your database user>';
-const DB_PASS = '<your database password>';
---
+++ app/src/config.sample.php
@@ -0,0 +1,5 @@
+<?php
+const DB_HOST = '<Your database host>';
+const DB_NAME = '<Your database name>';
+const DB_USER = '<Your database user>';
+const DB_PASS = '<Your database password>';
