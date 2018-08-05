b97e0a29f4c4262314cbb359ea99c576aaf52343
Chitoku <odango@chitoku.jp>
2018-08-05 19:52:24 +0900

    Write Twitter icon URLs to Redis

--- api/src/Model/Profile/Icon.php
+++ api/src/Model/Profile/Icon.php
@@ -8,11 +8,13 @@ use GuzzleHttp\ClientInterface;
 
 class Icon implements ProfileInterface
 {
+    public const CACHE_EXPIRE = 300;
     public static $default = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_200x200.png';
 
-    public function __construct(ClientInterface $client)
+    public function __construct(ClientInterface $client, \Redis $redis)
     {
         $this->client = $client;
+        $this->redis = $redis;
     }
 
     /**
@@ -23,6 +25,10 @@ class Icon implements ProfileInterface
     public function getAsync(string $screen_name): Promise\PromiseInterface
     {
         return Promise\coroutine(function () use ($screen_name) {
+            if ($url = $this->redis->get("icon:twitter:{$screen_name}")) {
+                return yield $url;
+            }
+
             try {
                 $target = "https://twitter.com/intent/user?screen_name={$screen_name}";
                 $response = yield $this->client->getAsync($target);
@@ -32,11 +38,18 @@ class Icon implements ProfileInterface
                     throw new \RuntimeException('No URL found');
                 }
                 [, $url] = $matches;
+
+                $this->save($screen_name, $url);
             } catch (\RuntimeException $e) {
                 $url = static::$default;
             }
 
             return yield $url ?? static::$default;
         });
+    }
+
+    protected function save(string $screen_name, string $url)
+    {
+        return $this->redis->set("icon:twitter:{$screen_name}", $url, static::CACHE_EXPIRE);
     }
 }
