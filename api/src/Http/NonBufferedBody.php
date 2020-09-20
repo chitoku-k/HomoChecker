0e101cc7ad6c8cedf383c1a0536d4010ff18bbf8
Chitoku <chitoku-k@users.noreply.github.com>
2020-09-20 23:54:16 +0900

    Support response body size in NonBufferedBody (#824)

---
+++ api/src/Http/NonBufferedBody.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace HomoChecker\Http;
+
+use Slim\Psr7\NonBufferedBody as NonBufferedBodyBase;
+
+class NonBufferedBody extends NonBufferedBodyBase
+{
+    protected $size = 0;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($string): int
+    {
+        $size = parent::write($string);
+        $this->size += $size;
+        return $size;
+    }
+}
