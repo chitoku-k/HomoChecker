ab33e8000ae090ff8d23bba5191e533c33cdad0a
Chitoku <odango@chitoku.jp>
2020-11-15 15:09:41 +0900

    Remove the usage of deprecated Promise functions

--- api/src/Contracts/Service/ProfileService.php
+++ api/src/Contracts/Service/ProfileService.php
@@ -3,16 +3,16 @@ declare(strict_types=1);
 
 namespace HomoChecker\Contracts\Service;
 
-use GuzzleHttp\Promise;
+use GuzzleHttp\Promise\PromiseInterface;
 
 interface ProfileService
 {
     /**
      * Get the profile icon of the account by the given screen name.
-     * @param  string                   $screen_name The screen name.
-     * @return Promise\PromiseInterface The promise that resolves with the URL string of the icon.
+     * @param  string           $screen_name The screen name.
+     * @return PromiseInterface The promise that resolves with the URL string of the icon.
      */
-    public function getIconAsync(string $screen_name): Promise\PromiseInterface;
+    public function getIconAsync(string $screen_name): PromiseInterface;
 
     /**
      * Get the default icon URL of the service.
