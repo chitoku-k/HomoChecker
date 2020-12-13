e264eb797f376700f7d2e80cdf860c36fecd818b
Chitoku <odango@chitoku.jp>
2020-12-14 03:15:19 +0900

    Fix a compatibility issue with IE 11 as of webpack 5

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
         path: path.join(__dirname, "/dist"),
         filename: "bundle.js",
     },
+    target: ["web", "es5"],
     module: {
         rules: [
             {
