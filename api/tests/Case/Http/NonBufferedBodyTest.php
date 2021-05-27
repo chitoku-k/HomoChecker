a5b478bcce36da160cd06eaf5067e6042da7db9a
Chitoku <odango@chitoku.jp>
2021-05-27 23:47:50 +0900

    Add tests for NonBufferedBody

---
+++ api/tests/Case/Http/NonBufferedBodyTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace HomoChecker\Test\Http;
+
+use HomoChecker\Http\NonBufferedBody;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Slim\Psr7\NonBufferedBody as Psr7NonBufferedBody;
+
+class NonBufferedBodyTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testWrite(): void
+    {
+        $base = m::mock('overload:' . Psr7NonBufferedBody::class);
+        $base->size = 0;
+        $base->shouldReceive('write')
+             ->withArgs(['test'])
+             ->andReturn(4);
+
+        $actual = new NonBufferedBody();
+
+        $this->assertEquals(0, $actual->getSize());
+        $actual->write('test');
+        $this->assertEquals(4, $actual->getSize());
+    }
+}
