f371511fd356e14275ba83b9f02e31593ece84af
Chitoku <chitoku-k@users.noreply.github.com>
2025-02-02 00:11:30 +0900

    Add a test case for invalid IDN (#2800)

--- api/tests/Case/Domain/StatusTest.php
+++ api/tests/Case/Domain/StatusTest.php
@@ -148,6 +148,55 @@ class StatusTest extends TestCase
         $this->assertEquals($duration, $actual->getResultArray()['duration']);
     }
 
+    public function testIncorrectIDN(): void
+    {
+        $id = 1;
+        $screen_name = 'homo';
+        $service = 'twitter';
+        $url = 'https://xn------.example.com';
+        $icon = 'https://img.example.com';
+        $status = ValidationResult::CONTAINS;
+        $http = '1.1';
+        $ip = '2001:db8::4545:1';
+        $duration = 1.14514;
+
+        $homo = new Homo(compact(
+            'id',
+            'screen_name',
+            'service',
+            'url',
+        ));
+
+        $result = new Result(compact(
+            'status',
+            'http',
+            'ip',
+            'url',
+            'duration',
+        ));
+
+        $actual = new Status(compact(
+            'homo',
+            'result',
+            'icon',
+        ));
+
+        $this->assertEquals($id, $actual->getHomo()->getId());
+        $this->assertEquals($screen_name, $actual->getHomo()->getScreenName());
+        $this->assertEquals($service, $actual->getHomo()->getService());
+        $this->assertEquals($url, $actual->getHomo()->getUrl());
+        $this->assertEquals($screen_name, $actual->getHomoArray()['screen_name']);
+        $this->assertEquals($service, $actual->getHomoArray()['service']);
+        $this->assertEquals($url, $actual->getHomoArray()['url']);
+        $this->assertEquals('', $actual->getHomoArray()['display_url']);
+        $this->assertEquals(true, $actual->getHomoArray()['secure']);
+        $this->assertEquals($status, $actual->getResultArray()['status']);
+        $this->assertEquals($http, $actual->getResultArray()['http']);
+        $this->assertEquals($ip, $actual->getResultArray()['ip']);
+        $this->assertEquals('', $actual->getResultArray()['url']);
+        $this->assertEquals($duration, $actual->getResultArray()['duration']);
+    }
+
     public function testIncorrectURL(): void
     {
         $id = 1;
